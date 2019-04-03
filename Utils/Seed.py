91e14bc2ba398749274a4d8b9aa85d00038ec084
Tyler Evans <ty.evans001@gmail.com>
2019-04-03 00:52:22 -0500

    Shhhh we're not using a seed

--- Utils/Seed.py
+++ Utils/Seed.py
@@ -6,7 +6,8 @@ class Seed:
 
     def __init__(self, seed=None, display_seed=True):
         if seed and display_seed:
-            print('Using existing seed:', seed)
+            #print('Using existing seed:', seed)
+            pass
         else:
             seed = np.random.randint(4190)
 
